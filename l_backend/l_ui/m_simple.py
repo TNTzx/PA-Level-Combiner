72b43ad9260bac85eb86b00705077fafb5ce3ca3
TNTzx <xanderandrewritua@gmail.com>
2022-09-07 17:00:11 +0800

    simple view start

---
+++ l_backend/l_ui/m_simple.py
@@ -0,0 +1,68 @@
+"""Contains the simple view."""
+
+
+import tkinter as tk
+import l_tkinter_utils
+import l_pa_cls_simple
+
+
+class SimpleView(tk.Frame):
+    """The simple view."""
+    def __init__(self, parent: tk.Widget):
+        super().__init__(parent, **l_tkinter_utils.FRAME_BORDER)
+        l_tkinter_utils.place_on_grid(self)
+        l_tkinter_utils.set_weights(self, y = (1 for _ in range(5)))
+
+        self.w_version_select = self.VersionSelect(self)
+        self.w_level_select = self.LevelSelect(self)
+        self.w_output = self.Output(self)
+        self.w_combine_controls = self.CombineControls(self)
+        self.w_misc_buttons = self.MiscButtons(self)
+
+    class VersionSelect(l_tkinter_utils.OptionMenuForm):
+        """The version select dropdown."""
+        def __init__(self, parent: tk.Widget):
+            super().__init__(
+                parent,
+                label_text = "PA Version",
+                options = [
+                    version.get_description() for version in l_pa_cls_simple.PAVersion.get_all_versions()
+                ],
+                initial = "Select PA Version..."
+            )
+            l_tkinter_utils.place_on_grid(self, coords = (0, 1))
+
+    class LevelSelect(tk.Frame):
+        """The level select."""
+        def __init__(self, parent: tk.Widget):
+            super().__init__(parent, **l_tkinter_utils.FRAME_BORDER)
+            l_tkinter_utils.place_on_grid(self, coords = (0, 2))
+            l_tkinter_utils.set_weights(self, x = (3, 1), y = (1, 2))
+
+            self.w_title = self.Title(self)
+            self.w_level_list = self.LevelList(self)
+            self.w_edit_buttons = self.EditButtons(self)
+
+        class Title(l_tkinter_utils.Title):
+            """The level select title."""
+            def __init__(self, parent: tk.Widget):
+                super().__init__(parent, title = "Level Folder List", title_size_mult = 0.5)
+                l_tkinter_utils.place_on_grid(self, span_set = (2, 1))
+
+        class LevelList(l_tkinter_utils.ScrolledListbox):
+            """The level list."""
+            def __init__(self, parent: tk.Widget):
+                super().__init__(parent)
+                l_tkinter_utils.place_on_grid(self, coords = (0, 1))
+
+        class EditButtons(l_tkinter_utils.AddRemoveEdit):
+            """The edit buttons."""
+            def __init__(self, parent: tk.Widget):
+                super().__init__(parent, item_label = "Level Folder")
+                l_tkinter_utils.place_on_grid(self, coords = (1, 1))
+
+    class Output(l_tkinter_utils.EntryBrowseForm):
+        """The output folder form."""
+        def __init__(self, parent: tk.Widget):
+            super().__init__(parent, label_text = "Output Level Folder")
+            l_tkinter_utils.place_on_grid(self, coords = (0, 3))
--- l_backend/l_ui/m_ui.py
+++ l_backend/l_ui/m_ui.py
@@ -2,6 +2,7 @@
 
 
 import tkinter as tk
+import tkinter.ttk as ttk
 
 import l_tkinter_utils
 import l_pa_cls_simple
@@ -16,11 +17,6 @@ class MainWindow(tk.Toplevel):
         l_tkinter_utils.window_center_to_screen(self)
 
         self.w_title = self.Title(self)
-        self.w_version_select = self.VersionSelect(self)
-        self.w_level_select = self.LevelSelect(self)
-        self.w_output = self.Output(self)
-        self.w_combine_controls = self.CombineControls(self)
-        self.w_misc_buttons = self.MiscButtons(self)
 
     class Title(l_tkinter_utils.Title):
         """The title."""
@@ -28,59 +24,17 @@ class MainWindow(tk.Toplevel):
             super().__init__(parent, title = "PA Level Combiner", description = "A program used to combine levels.")
             l_tkinter_utils.place_on_grid(self)
 
-    class VersionSelect(l_tkinter_utils.OptionMenuForm):
-        """The version select dropdown."""
+    class ViewManager(ttk.Notebook):
+        """The view manager."""
         def __init__(self, parent: tk.Widget):
-            super().__init__(
-                parent,
-                label_text = "PA Version",
-                options = [
-                    version.get_description() for version in l_pa_cls_simple.PAVersion.get_all_versions()
-                ],
-                initial = "Select PA Version..."
-            )
+            super().__init__(parent)
             l_tkinter_utils.place_on_grid(self, coords = (0, 1))
-
-    class LevelSelect(tk.Frame):
-        """The level select."""
-        def __init__(self, parent: tk.Widget):
-            super().__init__(parent, **l_tkinter_utils.FRAME_BORDER)
-            l_tkinter_utils.place_on_grid(self, coords = (0, 2))
-            l_tkinter_utils.set_weights(self, x = (3, 1), y = (1, 2))
-
-            self.w_title = self.Title(self)
-            self.w_level_list = self.LevelList(self)
-            self.w_edit_buttons = self.EditButtons(self)
-
-        class Title(l_tkinter_utils.Title):
-            """The level select title."""
-            def __init__(self, parent: tk.Widget):
-                super().__init__(parent, title = "Level Folder List", title_size_mult = 0.5)
-                l_tkinter_utils.place_on_grid(self, span_set = (2, 1))
-
-        class LevelList(l_tkinter_utils.ScrolledListbox):
-            """The level list."""
-            def __init__(self, parent: tk.Widget):
-                super().__init__(parent)
-                l_tkinter_utils.place_on_grid(self, coords = (0, 1))
-
-        class EditButtons(l_tkinter_utils.AddRemoveEdit):
-            """The edit buttons."""
-            def __init__(self, parent: tk.Widget):
-                super().__init__(parent, item_label = "Level Folder")
-                l_tkinter_utils.place_on_grid(self, coords = (1, 1))
-
-    class Output(l_tkinter_utils.EntryBrowseForm):
-        """The output folder form."""
-        def __init__(self, parent: tk.Widget):
-            super().__init__(parent, label_text = "Output Level Folder")
-            l_tkinter_utils.place_on_grid(self, coords = (0, 3))
 
     class CombineControls(tk.Frame):
         """Contains the combining controls."""
         def __init__(self, parent: tk.Widget):
             super().__init__(parent, **l_tkinter_utils.FRAME_BORDER)
-            l_tkinter_utils.place_on_grid(self, coords = (0, 4))
+            l_tkinter_utils.place_on_grid(self, coords = (0, 2))
             l_tkinter_utils.set_weights(self, x = (1, 1))
 
             self.w_options = self.Options(self)
@@ -104,7 +58,7 @@ class MainWindow(tk.Toplevel):
         """Contains all the miscellaneous buttons."""
         def __init__(self, parent: tk.Widget):
             super().__init__(parent, **l_tkinter_utils.FRAME_BORDER)
-            l_tkinter_utils.place_on_grid(self, coords = (0, 5))
+            l_tkinter_utils.place_on_grid(self, coords = (0, 3))
             l_tkinter_utils.set_weights(self, x = (1, 1, 1))
 
             self.w_instructions = self.Instructions(self)
