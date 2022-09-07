43391719999e5470e1364a469efa47031071aa99
TNTzx <xanderandrewritua@gmail.com>
2022-09-08 00:41:13 +0800

    forgot to bind lol

--- l_backend/l_ui/m_simple.py
+++ l_backend/l_ui/m_simple.py
@@ -51,7 +51,11 @@ class SimpleView(tk.Frame):
 
             self.w_title = self.Title(self)
             self.w_level_list = self.LevelList(self)
+
             self.w_edit_buttons = self.EditButtons(self)
+            self.w_edit_buttons.add = self.add_level_folder
+            self.w_edit_buttons.edit = self.edit_level_folder
+            self.w_edit_buttons.remove = self.remove_level_folder
 
             self._level_folder_paths = level_folder_paths
 
@@ -76,7 +80,7 @@ class SimpleView(tk.Frame):
 
         def update_listbox(self):
             """Updates the listbox with the level folder paths."""
-            l_tkinter_utils.listbox_update(self, self._level_folder_paths)
+            l_tkinter_utils.listbox_update(self.w_level_list.w_listbox, self._level_folder_paths)
 
         @staticmethod
         def _update_wrapper(func: typ.Callable):
@@ -93,7 +97,7 @@ class SimpleView(tk.Frame):
             """Adds a level folder."""
             path = tkfd.askdirectory(title = "Select a folder containing a level")
             self._level_folder_paths.append(path)
-            self.update_listbox()
+
 
         @_update_wrapper
         def edit_level_folder(self):
@@ -101,6 +105,7 @@ class SimpleView(tk.Frame):
             selected = l_tkinter_utils.listbox_get_selected(self.w_level_list.w_listbox, self._level_folder_paths)
             if len(selected) != 1:
                 l_tkinter_utils.error_messagebox(self.w_parent, "You must select one and only one item to edit.")
+                return
             selected: str = selected[0]
 
             class EntryBrowseForm(l_tkinter_utils.EntryBrowseForm):
@@ -123,11 +128,12 @@ class SimpleView(tk.Frame):
 
 
         @_update_wrapper
-        def delete_level_folder(self):
+        def remove_level_folder(self):
             """Deletes the selected level folder."""
             selected_items = l_tkinter_utils.listbox_get_selected(self.w_level_list.w_listbox, self._level_folder_paths)
             if len(selected_items) == 0:
                 l_tkinter_utils.error_messagebox(self.w_parent, "You must select at least one item to delete.")
+                return
 
             confirm = l_tkinter_utils.messagebox(
                 self.w_parent,
