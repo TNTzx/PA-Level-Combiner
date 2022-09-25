9eba9c50e001d01354af8e00fb469be9a41d4389
TNTzx <xanderandrewritua@gmail.com>
2022-09-25 21:35:01 +0800

    how

--- l_backend/l_ui/l_extra_info/m_instructions.py
+++ l_backend/l_ui/l_extra_info/m_instructions.py
@@ -31,9 +31,49 @@ class Instructions(tk.Toplevel):
             super().__init__(parent)
             l_tkinter_utils.place_on_grid(self, coords = (0, 1))
 
+            self.frame_infos = [
+                l_tkinter_utils.NotebookFrameInfo("Workflow", Workflow(self))
+            ]
+
     class Close(tk.Button):
         """The close button."""
         def __init__(self, parent: tk.Widget):
             super().__init__(parent, text = "Close")
             l_tkinter_utils.place_on_grid(self, coords = (0, 2))
             l_tkinter_utils.set_font(self)
+
+
+class Workflow(l_tkinter_utils.ScrollableFrame):
+    """Workflow tab"""
+    def __init__(self, parent: tk.Widget):
+        super().__init__(parent)
+        l_tkinter_utils.place_on_grid(self)
+
+        self.w_label_format = self.LabelFormat(self.w_canvas.w_frame)
+
+    class LabelFormat(l_tkinter_utils.LabelFormat):
+        """The label."""
+        def __init__(self, parent: tk.Widget):
+            super().__init__(parent)
+            l_tkinter_utils.place_on_grid(self)
+            l_tkinter_utils.set_font(self)
+
+            texts = [
+                l_tkinter_utils.LabelFormatText(
+                    "1. Select the PA version of the levels that you'll use to combine."
+                ),
+                l_tkinter_utils.LabelFormatText(
+                    "2. Add your level folders using the \"Add Level Folder\" button. Edit and delete if necessary."
+                ),
+                l_tkinter_utils.LabelFormatText(
+                    "3. Create a folder to store the combined level, then use the \"Browse\" button to set the output folder to that folder."
+                ),
+                l_tkinter_utils.LabelFormatText(
+                    "4. Set other combine options in the \"Advanced\" tab."
+                ),
+                l_tkinter_utils.LabelFormatText(
+                    "5. Press the combine button!"
+                )
+            ]
+
+            self.add_texts(texts)
