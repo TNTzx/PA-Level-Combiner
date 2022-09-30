d3d31e5ac3b6fe198638f2ed34f305626c546e0d
TNTzx <xanderandrewritua@gmail.com>
2022-09-30 21:50:24 +0800

    build code

--- make_zip/build.py
+++ make_zip/build.py
@@ -28,43 +28,43 @@ def get_all_file_paths_in_folder(folder_path: str):
 build_version = input("Input level version (no v): ")
 OUTPUT_ZIP = osp.join(OUTPUT_DIR, f"PA Level Combiner v{build_version}.zip")
 
-zip_file = zipf.ZipFile(OUTPUT_ZIP, "w")
 
 
-def write_folder(folder_path: str, zip_folder_path: str = None, path_exclude_regex: str = r"__pycache__", verbose: bool = True):
-    """Writes a folder to the zip file."""
-    if zip_folder_path is None:
-        zip_folder_path = folder_path.split(os.sep)[-1]
+with zipf.ZipFile(OUTPUT_ZIP, "w") as zip_file:
+    def write_folder(folder_path: str, zip_folder_path: str = None, path_exclude_regex: str = r"__pycache__", verbose: bool = True):
+        """Writes a folder to the zip file."""
+        if zip_folder_path is None:
+            zip_folder_path = folder_path.split(os.sep)[-1]
 
-    path_exclude_compile = re.compile(path_exclude_regex)
+        path_exclude_compile = re.compile(path_exclude_regex)
 
-    file_paths = get_all_file_paths_in_folder(folder_path)
-    for file_path in file_paths:
-        if bool(path_exclude_compile.search(file_path)):
-            continue
+        file_paths = get_all_file_paths_in_folder(folder_path)
+        for file_path in file_paths:
+            if bool(path_exclude_compile.search(file_path)):
+                continue
 
-        file_split_path = file_path.split(os.sep)
-        folder_split_path = folder_path.split(os.sep)
+            file_split_path = file_path.split(os.sep)
+            folder_split_path = folder_path.split(os.sep)
 
-        file_split_path = file_split_path[-(len(file_split_path) - len(folder_split_path)):]
+            file_split_path = file_split_path[-(len(file_split_path) - len(folder_split_path)):]
 
-        file_split_path.insert(0, zip_folder_path)
-        final_zip_folder_path = os.path.join(*file_split_path)
+            file_split_path.insert(0, zip_folder_path)
+            final_zip_folder_path = os.path.join(*file_split_path)
 
-        if verbose:
-            print(f"Writing: {file_path}")
+            if verbose:
+                print(f"Writing: {file_path}")
 
-        zip_file.write(file_path, final_zip_folder_path)
-
-
-def get_relative_dir(folder_name: str):
-    """Gets the relative directory path."""
-    return osp.join(REPO_DIR, folder_name)
+            zip_file.write(file_path, final_zip_folder_path)
 
 
-write_folder(get_relative_dir(".py_embedded"))
-write_folder(get_relative_dir("code"), path_exclude_regex = r"(__pycache__)|(archive)|(testcode.py)")
-write_folder(get_relative_dir("setup"))
-zip_file.write(get_relative_dir("README.md"), "README.md")
+    def get_relative_dir(folder_name: str):
+        """Gets the relative directory path."""
+        return osp.join(REPO_DIR, folder_name)
+
+
+    write_folder(get_relative_dir(".py_embedded"))
+    write_folder(get_relative_dir("code"), path_exclude_regex = r"(__pycache__)|(archive)|(testcode.py)")
+    write_folder(get_relative_dir("setup"))
+    zip_file.write(get_relative_dir("README.md"), "README.md")
 
 input("Finished. Press Enter to Exit.")
