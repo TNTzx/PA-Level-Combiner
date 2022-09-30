86aebb67a8b225691eb144540692a87987667d5a
TNTzx <xanderandrewritua@gmail.com>
2022-09-30 19:01:47 +0800

    new setup stuff

--- code/run.bat
+++ code/run.bat
@@ -2,4 +2,15 @@
 color 0a
 title PA Level Combiner
 
-".py_embedded/python.exe" main.py
+
+set codepath=%~dp0
+
+call :getpath "%codepath%..\.py_embedded\pythonw.exe"
+echo %interpreterpath%
+
+"%interpreterpath%" "%codepath%main.py"
+
+
+:getpath
+    set interpreterpath=%~f1
+    exit /b
